9e1b7ba538964bb15f9285cb2818a23fed275437
Jeff Gerber <jallengerber@gmail.com>
2015-09-23 19:38:19 -0700

    fixed POST test added names/* to .gitignore

--- test/router_test.js
+++ test/router_test.js
@@ -36,7 +36,7 @@ describe('basic server', function() {
     it('should respond to a request', function(done) {
         chai.request('localhost:3000')
             .post('/names')
-            .send('')
+            .send({msg: 'Goodbye Cruel World!'})
             .then(function(err, res) {
                 expect(err).to.eql(null);
                 expect(res.status).to.eql(200);
